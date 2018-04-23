43664b8d95cdf424cdc50b0ea22533fcc2dcb6ef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# DateEdit - How to enlarge dates in calendars on hovering with the mouse
+
+
+<p>This example demonstrates how to enlarge dates in the DateEdit's calendars when hovering the mouse.</p>
+
+
+<h3>Description</h3>
+
+<p>Starting with 15.2, DateEdit supports <strong>CalendarAppearance</strong>&nbsp;settings. To change&nbsp;the&nbsp;date appearance when you hover a certain date, you can set necessary adjustment in the RepositporyItemDateEdit.<strong>CalendarAppearance.DayCellHighlighted&nbsp;</strong>property.</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraLayout
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.Utils.Drawing
+Imports DevExpress.XtraEditors.Repository
+
+
+Namespace WindowsApplication3
+	Partial Public Class Form1
+		Inherits XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' TODO: This line of code loads data into the 'nwindDataSet.Orders' table. You can move, or remove it, as needed.
+			Me.ordersTableAdapter.Fill(Me.nwindDataSet.Orders)
+		  dateEdit1.Properties.AppearanceCalendar.DayCellHighlighted.Font = New System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+		  dateEdit1.Properties.AppearanceCalendar.DayCellHighlighted.Options.UseFont = True
+		 repositoryItemDateEdit1.AppearanceCalendar.DayCellHighlighted.Font = New System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+		 repositoryItemDateEdit1.AppearanceCalendar.DayCellHighlighted.Options.UseFont = True
+
+		End Sub
+
+	End Class
+End Namespace
